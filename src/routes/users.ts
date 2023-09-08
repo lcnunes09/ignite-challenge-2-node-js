7d88f20db5cd647c2cb2370d1855aba9ef172f45
Luciana Nunes <lcnunes09@gmail.com>
2023-09-07 20:33:42 -0400

    feat: create users

---
+++ src/routes/users.ts
@@ -0,0 +1,28 @@
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { randomUUID } from 'node:crypto'
+import { knex } from '../database'
+
+
+export async function usersRoutes(app: FastifyInstance) {
+    app.post('/', async (request, reply) => {
+        const createUsersSchema = z.object({
+            email: z.string().email(),
+            name: z.string(),
+            image: z.string().url(),
+        })
+            
+        const { email, name, image } = createUsersSchema.parse(request.body)
+
+        await knex('users')
+            .insert({
+                id: randomUUID(),
+                email,
+                name,
+                image,
+                created_at: new Date(),
+            })
+
+        return reply.status(201).send()
+    })
+}
